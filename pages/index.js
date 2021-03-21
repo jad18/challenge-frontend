91e245151db56cb7c33c9e5507f75fbdaa124cbb
Jackson DeWitt <jacksonadewitt@gmail.com>
2021-03-21 02:07:40 -0700

    Updated state handling to properly accept change

--- pages/index.js
+++ pages/index.js
@@ -2,38 +2,28 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Avatars from './Avatars'
 import getCurrentDay, { formatAndSetDate } from './Dates'
-import { useState } from 'react';
+import React from 'react'
 
-export default function Home() {
-  const [state, setState] = useState(
-    { level: 0, name: "", birthday: "", pronouns: "" }
-  );
 
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Conversation</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+export default class Home extends React.Component {
+  constructor()
+  {
+    super();
+    this.state = {
+      level: 0,
+      name: "", 
+      birthday: "", 
+      pronouns: ""
+    };
 
-      <main className={styles.main}>
-        <Avatars index1={1} index2={0} />
-        <hr className={styles.horz_line}/>
+    this.getBody = this.getBody.bind(this);
+    this.getNameFooter = this.getNameFooter.bind(this);
+    this.setName = this.setName.bind(this);
+  }
 
-      {getBody(state)}
-        
-      </main>
-      {(state.level == 0)
-        ? getNameFooter(state, setState)
-        : <div></div>
-      }
-    </div>
-  )
-}
-
-function getBody(state)
-{
-  return(
+  getBody()
+  {
+    return(
     <div className={styles.main_body}>
       <header className={styles.bot_entry}><u>Hudello</u></header>
       <p className={styles.bot_entry}>
@@ -43,13 +33,13 @@ function getBody(state)
       </p>
       <br/>
       <b className={styles.bot_entry}>What's your name?</b>
-      {(state.level >= 1)
+      {(this.state.level >= 1)
         ? <div>
-            <p className={styles.user_entry}>{state.name}</p>
+            <p className={styles.user_entry}>{this.state.name}</p>
             <br/><br/><br/>
             <header className={styles.bot_entry}><u>Hudello</u></header>
             <p className={styles.bot_entry}>
-              Hi, nice to meet you <b>{state.name}</b>!
+              Hi, nice to meet you <b>{this.state.name}</b>!
             </p>
             <p className={styles.bot_entry}>
               Since this is your first time meeting me, let's say my birthday
@@ -62,9 +52,9 @@ function getBody(state)
         : <div></div>
       }
 
-      {(state.level >= 2)
+      {(this.state.level >= 2)
         ? <div>
-            <p className={styles.user_entry}>{state.birthday}</p>
+            <p className={styles.user_entry}>{this.state.birthday}</p>
             <br/><br/><br/>
             <header className={styles.bot_entry}><u>Hudello</u></header>
             <p className={styles.bot_entry}>
@@ -75,18 +65,18 @@ function getBody(state)
         : <div></div>
       }
 
-      {(state.level >= 3)
-        ? <p className={styles.user_entry}>{state.pronouns}</p>
+      {(this.state.level >= 3)
+        ? <p className={styles.user_entry}>{this.state.pronouns}</p>
         : <div></div>
 
       }
 
     </div>
-  );
-}
+    );
+  }
 
-function getNameFooter(state, setState)
-{
+  getNameFooter()
+  {
   return(
     <div className={styles.main_footer}>
       <input
@@ -96,21 +86,42 @@ function getNameFooter(state, setState)
         </input>
       <button
         className={styles.submit_name_button}
-        onClick={() => setName(state, setState)}>
+        onClick={this.setName}>
         {'⪢'}
       </button>
     </div>
-  );
-}
+    );
+  }
 
-function setName(state, setState)
-{
-  const text = document.getElementById("name_textbox").value;
-  console.log(state);
-  if(text != undefined && text.length > 0)
+  setName()
   {
-    state.name = text;
-    state.level = state.level + 1;
-    setState(state);
+    const text = document.getElementById("name_textbox").value;
+    console.log(this.state);
+    if(text != undefined && text.length > 0)
+      this.setState({ name: text, level: this.state.level + 1 });
+  }
+
+  render()
+  {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Conversation</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <Avatars index1={1} index2={0} />
+        <hr className={styles.horz_line}/>
+
+      {this.getBody()}
+        
+      </main>
+      {(this.state.level == 0)
+        ? this.getNameFooter()
+        : <div></div>
+      }
+    </div>
+  );
   }
 }
